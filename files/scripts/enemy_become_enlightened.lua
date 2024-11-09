90965fb6ed77d153e03434cb872beae77dfbf66d
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2024-11-09 16:09:00 +1100

    wip mod desc, enlightenment is more common without hope

--- files/scripts/enemy_become_enlightened.lua
+++ files/scripts/enemy_become_enlightened.lua
@@ -7,11 +7,22 @@ local x, y = EntityGetTransform(entity)
 
 math.randomseed(x, y + GameGetFrameNum())
 
-if GetDLDifficulty() >= 3 then
+local difficulty = GetDLDifficulty()
+
+if difficulty >= 3 then
+
     if not EntityHasTag(entity, "DL_enlightened_checked") then
-        if math.random(1, 15) == 2 then
-            CreateEnlightenedEnemy(entity)
+        if difficulty == 4 then
+            if math.random(1, 10) == 2 then
+                CreateEnlightenedEnemy(entity)
+            end
+            EntityAddTag(entity, "DL_enlightened_checked")
         end
-        EntityAddTag(entity, "DL_enlightened_checked")
+        if difficulty == 3 then
+            if math.random(1, 15) == 2 then
+                CreateEnlightenedEnemy(entity)
+            end
+            EntityAddTag(entity, "DL_enlightened_checked")
+        end
     end
 end
